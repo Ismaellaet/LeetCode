f726e3509479e5fa3851e752d5846d280fb697b0
ismael <ismaelabashi@hotmail.com>
2022-07-11 20:13:22 -0300

    Reset Programming skill II

--- Study Plans/Programmin Skills/II/Day1/Implement strStr()/result.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {string} haystack
- * @param {string} needle
- * @return {number}
- */
-var strStr = function(haystack, needle) {
-    const sub = haystack.indexOf(needle);
-    return sub
-};
--- Study Plans/Programmin Skills/II/Day2/Repeated Substring Pattern/result.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
-var repeatedSubstringPattern = function(s) {
-    let string = s + s;
-
-    return string.substring(1, string.length-1)
-};
